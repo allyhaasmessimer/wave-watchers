c4b6c751a25879e9a156ed28ca5922c1fcd0c689
allyhaasmessimer <allyhaas@gmail.com>
2023-12-04 17:08:50 -0600

    more css and html stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,102 @@ import MapContainer from "./components/map-container";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h2>Barrels Await, Wave Watchers Navigate.</h2>
-                <h1>Wave Watchers</h1>
-                <div id="header_image">
-                    <img src="/header.png" alt="people surfing" />
+            <section id="header">
+                <header className="major">
+                    <h1>Wave Watchers</h1>
+                    <p>Barrels Await, Wave Watchers Navigate.</p>
+                </header>
+                <div className="container">
+                    <ul className="actions special">
+                        <li>
+                            <a href="#one" className="button primary scrolly">
+                                Begin
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <div id="blank_space">
-                    <h2>Enter your location</h2>
-                    <form action="" method="post">
-                        <label>City:</label>
-                        <input
-                            type="text"
-                            id="city"
-                            name="city"
-                            placeholder="Enter your city"
-                            required
-                        />
+            </section>
 
-                        <label>Country:</label>
-                        <input
-                            type="text"
-                            id="country"
-                            name="country"
-                            placeholder="Enter your country"
-                            required
-                        />
-
-                        <button type="submit">Submit</button>
-                    </form>
+            <section id="one" className="main special">
+                <div className="container">
+                    <span className="image fit primary">
+                        <img src="images/pic01.jpg" alt="" />
+                    </span>
+                    <div className="content">
+                        <header className="major">
+                            <h2>Who I am</h2>
+                        </header>
+                        <img src="./images/header1.png" alt="" />
+                    </div>
+                    <a href="#two" className="goto-next scrolly">
+                        Next
+                    </a>
                 </div>
-            </header>
+            </section>
+
+            <section id="two" className="main special">
+                <div className="container">
+                    <span className="image fit primary">
+                        <img src="images/pic02.jpg" alt="" />
+                    </span>
+                    <div className="content">
+                        <header className="major">
+                            <h2>Stuff I do</h2>
+                        </header>
+                        <p>
+                            Consequat sed ultricies rutrum. Sed adipiscing eu
+                            amet interdum lorem blandit vis ac commodo aliquet
+                            vulputate.
+                        </p>
+                        <ul className="icons-grid">
+                            <li>
+                                <span className="icon solid major fa-camera-retro"></span>
+                                <h3>Magna Etiam</h3>
+                            </li>
+                            <li>
+                                <span className="icon solid major fa-pencil-alt"></span>
+                                <h3>Lorem Ipsum</h3>
+                            </li>
+                            <li>
+                                <span className="icon solid major fa-code"></span>
+                                <h3>Nulla Tempus</h3>
+                            </li>
+                            <li>
+                                <span className="icon solid major fa-coffee"></span>
+                                <h3>Sed Feugiat</h3>
+                            </li>
+                        </ul>
+                    </div>
+                    <a href="#three" className="goto-next scrolly">
+                        Next
+                    </a>
+                </div>
+            </section>
+
+            <section id="three" className="main special">
+                <div className="container">
+                    <span className="image fit primary">
+                        <img src="images/pic03.jpg" alt="" />
+                    </span>
+                    <div className="content">
+                        <header className="major">
+                            <h2>One more thing</h2>
+                        </header>
+                        <p>
+                            Aliquam ante ac id. Adipiscing interdum lorem
+                            praesent fusce pellentesque arcu feugiat. Consequat
+                            sed ultricies rutrum. Sed adipiscing eu amet
+                            interdum lorem blandit vis ac commodo aliquet
+                            integer vulputate phasellus lorem ipsum dolor lorem
+                            magna consequat sed etiam adipiscing interdum.
+                        </p>
+                    </div>
+                    <a href="#footer" className="goto-next scrolly">
+                        Next
+                    </a>
+                </div>
+            </section>
+
             {/* <MapContainer
                 markerPosition={{
                     lat: 30.393181,
